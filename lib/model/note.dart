4df53e48e8f51fc2a2e63bc21b7a28052dc977f8
Creeprus <isip_l.k.kornienko>
2023-01-24 09:41:36 +0300

    refactoring

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -1,19 +1,16 @@
 import 'package:conduit/conduit.dart';
 
-class Note extends ManagedObject<_Note> implements _Note{}
+class Note extends ManagedObject<_Note> implements _Note {}
 
-
-class _Note
-{
+class _Note {
   @primaryKey
   int? id;
-  @Column(unique:true,indexed:true)
+  @Column(unique: true, indexed: true)
   String? noteName;
-  @Column(unique:false,indexed:true)
+  @Column(unique: false, indexed: true)
   String? noteCategory;
-  @Column(unique:false,indexed:true)
+  @Column(unique: false, indexed: true)
   String? noteDateCreated;
-  @Column(unique:false,indexed:true)
+  @Column(unique: false, indexed: true)
   String? noteDateChanged;
-
 }
--- lib/utils/app_utils.dart
+++ lib/utils/app_utils.dart
@@ -1,12 +1,11 @@
-
 import 'dart:io';
 import 'package:conduit/conduit.dart';
 import 'package:jaguar_jwt/jaguar_jwt.dart';
 
-abstract class AppUtils{
+abstract class AppUtils {
   const AppUtils._();
 
-   static int getIdFromToken(String token) {
+  static int getIdFromToken(String token) {
     try {
       final key = Platform.environment["SECRET_KEY"] ?? 'SECRET_KEY';
       final jwtCLaim = verifyJwtHS256Signature(token, key);
@@ -15,7 +14,8 @@ abstract class AppUtils{
       rethrow;
     }
   }
-   static int getIdFromHeader(String header) {
+
+  static int getIdFromHeader(String header) {
     try {
       final token = const AuthorizationBearerParser().parse(header);
       final id = getIdFromToken(token ?? "");
