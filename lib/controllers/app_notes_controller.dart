9d472287517db4b5f0d7bb6762b335a5999be52b
Creeprus <isip_l.k.kornienko>
2023-01-24 09:35:49 +0300

    pagination complete

--- lib/controllers/app_notes_controller.dart
+++ lib/controllers/app_notes_controller.dart
@@ -11,50 +11,37 @@ class AppNotesConttoller extends ResourceController {
   AppNotesConttoller(this.managedContext);
 
   final ManagedContext managedContext;
-   
+
   @Operation.get()
   Future<Response> getNotes(
-       @Bind.query('page') int page,
+    @Bind.query('page') int page,
     @Bind.query('amount') int amount,
   ) async {
     try {
-    
-   
       // Получаем id пользователя
       // Была создана новая функция ее нужно реализоваться для просмотра функции нажмите на картинку
       final id = amount;
       // Получаем данные пользователя по его id
-      final qGetAll= await Query<Note>(managedContext)
-     ..fetchLimit=id;
-      final notes=await qGetAll.fetch();
+      final qGetAll = await Query<Note>(managedContext)
+        ..offset = page
+        ..fetchLimit = id;
 
-//     var map2 = {};
-//     notes.forEach((customer) => map2[note.noteName] = customer.noteName
-//     );
-//     notes.forEach((customer) => map2[note.noteCategory] = customer.noteCategory
-//     );
-// notes.forEach((customer) => map2[note.noteDateChanged] = customer.noteDateChanged
-//     );
-//     notes.forEach((customer) => map2[note.noteDateCreated] = customer.noteDateCreated
-//     );
-//      notes.forEach((customer) => map2[note.id] = customer.id
-//     );
-//     print(map2);
-var map2=notes.map((e){ return
-{
-"id": e.id,
-"noteName": e.noteName,
-	"noteCategory": e.noteCategory,
-	"noteDateCreated": e.noteDateCreated,
-	"noteDateChanged": e.noteDateChanged
-};
-}
+      final notes = await qGetAll.fetch();
 
-).toList();
-       return AppResponse.ok(
-       //message: 'Успешное получение заметки', body: notes..forEach((Note element) { element.backing.contents;}));
-         message: 'Успешное получение заметки', body: map2);
-     //message: 'Успешное получение заметки', body: notes[1].backing.contents);
+      var map2 = notes.map((e) {
+        return {
+          "id": e.id,
+          "noteName": e.noteName,
+          "noteCategory": e.noteCategory,
+          "noteDateCreated": e.noteDateCreated,
+          "noteDateChanged": e.noteDateChanged
+        };
+      }).toList();
+      return AppResponse.ok(
+          //message: 'Успешное получение заметки', body: notes..forEach((Note element) { element.backing.contents;}));
+          message: 'Успешное получение заметки',
+          body: map2);
+      //message: 'Успешное получение заметки', body: notes[1].backing.contents);
 
     } catch (e) {
       return AppResponse.serverError(e, message: 'Ошибка получения заметки');
